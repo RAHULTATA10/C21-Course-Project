e5261fc29de0ba8091260b90e02b4ea65dfd0bca
RAHUL <56194955+RAHULTATA10@users.noreply.github.com>
2019-12-28 14:54:43 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,30 @@
-var Rect, Squar, Circl;
+var fixedRect, movingRect;
 
 function setup() {
-    
-  createCanvas(800,400);
-    
-  Rect=createSprite(200,200,50,80); 
-  Rect.shapeColor="green";
-    
-  Squar=createSprite(325,325,30,30);
-  Squar.shapeColor="red";
-    
-  Circl=createSprite(250,250,80,30);
-  Circl.shapeColor="blue";
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
 }
 
 function draw() {
-  background(255,255,255); 
-  
-  drawSprites();
+  background(0,0,0);  
+
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2) {
+    movingRect.velocityX = movingRect.velocityX * (-1);
+    fixedRect.velocityX = fixedRect.velocityX * (-1);
   }
+  if (movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
+    movingRect.velocityY = movingRect.velocityY * (-1);
+    fixedRect.velocityY = fixedRect.velocityY * (-1);
+  }
+  drawSprites();
+}
